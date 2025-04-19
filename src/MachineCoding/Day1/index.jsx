c8364c7387327cdf2180d167e8aa8008425bd856
Pankaj Gusain <gusainpankaj1999@gmail.com>
2025-04-19 17:44:50 +0530

    Refactor Counter component: enhance styling and implement reducer logic for increment, decrement, and reset actions

--- src/MachineCoding/Day1/index.jsx
+++ src/MachineCoding/Day1/index.jsx
@@ -2,35 +2,50 @@ import { useReducer } from "react";
 import './index.css';
 
 
-const count = 0 ;
+const count = 0;
 
-const reducer = (state , action) => {
-    switch(action.type){
-        case "increment" : 
-            return state+1;
+const reducer = (state, action) => {
+    switch (action.type) {
+        case "increment":
+            return state + 1;
 
-        case "decrement" :  
-            return state == 0 ? state : state-1;
+        case "decrement":
+            return state == 0 ? state : state - 1;
 
         case "reset":
-            return 0 ;
+            return 0;
 
-        default :
+        default:
             return state;
     }
 }
 
-const Counter = () =>{
+const Counter = () => {
 
-    const [state , dispatch ] = useReducer(reducer , count);
+    const [state, dispatch] = useReducer(reducer, count);
+
+    const handlekeyDown = (e)=>{
+        // isTrusted 
+        console.log(e.key);
+        if(e.key == "enter"){
+            dispatch({type:"increment"})
+        }
+    }
 
     return (
         <div className="counter">
-            Counter : {state}
-            <div>
-                <button onClick={()=>dispatch({type:'increment'})}>Increment</button>
-                <button onClick={()=>dispatch({type:'decrement'})}>Decrement</button>
-                <button disabled={state==0} onClick={()=>dispatch({type:'reset'})}>Reset</button>
+            <div className="counter_value">
+                <span>Counter : {state}</span>
+            </div>
+            <div className="counter_buttons">
+                <button 
+                    onClick={() => dispatch({ type: 'increment' })} 
+                    onKeyDown={(e)=> handlekeyDown(e)}
+                >
+                    Increment
+                </button>
+                <button onClick={() => dispatch({ type: 'decrement' })}>Decrement</button>
+                <button disabled={state == 0} onClick={() => dispatch({ type: 'reset' })}>Reset</button>
             </div>
 
         </div>
