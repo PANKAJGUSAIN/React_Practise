26d0ff3739048fde3e27dc5852561a508d364798
Pankaj Gusain <gusainpankaj1999@gmail.com>
2025-04-20 13:05:02 +0530

    Implement Drag and Drop for sorting

---
+++ src/MachineCoding/Day4/index.js
@@ -0,0 +1,79 @@
+import { useState } from 'react';
+import './index.css';
+
+const DragAndDrop = () =>{
+
+    const [items, setItems] = useState([
+        "Item 1",
+        "Item 2",
+        "Item 3",
+        "Item 4",
+        "Item 5",
+      ]);
+
+    const [draggedIndex, setDraggedIndex] = useState(null);
+    const [draggedOver, setDrageedOver] = useState(null);
+    const [dragEnd , setDragEnd] = useState(false);
+
+    const handleDragStart = (e , id) =>{
+        setDraggedIndex(id);
+        setDragEnd(false);
+    }
+
+    const handleDragOver = (e , id) =>{
+        e.preventDefault(); // this is needed  else you won't be able to drop 
+        console.log('dragging over' , id)
+        setDrageedOver(id);
+    }
+
+    const handleDrop = (e , id) =>{
+        console.log("DROP" , id);
+        let newOrderItems = [...items];
+        [newOrderItems[draggedIndex] , newOrderItems[id]] = [newOrderItems[id], newOrderItems[draggedIndex]]
+        setDrageedOver(null);
+        setDraggedIndex(null);
+        setItems(newOrderItems);
+        setDragEnd(true);
+    }
+
+    const handleDragEnd = (e , id)=>{
+        if(!dragEnd){
+            setDraggedIndex(null);
+            setDrageedOver(null);
+        }
+    }
+
+
+
+    return (
+        <div style={{height:"90%" , width:"100%"}}>
+            Drag And Drop
+
+            <div className='DroppingArea'>
+                {
+                    items.map((item , id) =>(
+                        <div key={id} 
+                            className='DragItem'
+                            draggable 
+                            onDragStart={(e)=>handleDragStart(e , id)}
+                            onDragOver={(e)=>handleDragOver(e , id)}
+                            onDrop={(e)=>handleDrop(e , id)}
+                            onDragEnd={(e)=>handleDragEnd(e , id)}
+                            style={draggedIndex == id ? {
+                                border:'1px dashed white',
+                                opacity:0.5
+                            }:{}}
+                        >
+                            {item}
+                        </div>
+                    ))
+                }
+                
+            </div>
+
+
+        </div>
+    )
+}
+
+export default DragAndDrop
--- src/app.jsx
+++ src/app.jsx
@@ -1,25 +1,35 @@
 import { BrowserRouter as Router, Route, Routes, Link } from 'react-router-dom';
-import { lazy, Suspense } from 'react';
+import React, { lazy, Suspense } from 'react';
 import './index.css';
 
 const Counter = lazy(()=> import('./MachineCoding/Day1/index'));
 const TodoApp = lazy(()=> import('./MachineCoding/Day2/index'));
 const ShoppingCart = lazy(()=> import('./MachineCoding/Day3/index'));
+const DragAndDrop = lazy(()=>import('./MachineCoding/Day4/index'));
+
+// const component = {
+//     Counter : lazy(() => import('./MachineCoding/Day1/index')),
+// }
 
 
 const App = () => {
     return (
         <Suspense fallback={<div>Loading...</div>}>
-            <div>
+            <div style={{height:"100%" , width:"100%"}}>
+                {/* // dynamic render */}
+                {/* <component.Counter /> */}
                 <nav className='Navbar'>
                     <Link to="/MachineCode1">Counter</Link>
                     <Link to="/MachineCode2">Todo App</Link>
                     <Link to="/MachineCode3">Shopping Cart</Link>
+                    <Link to="/MachineCode4">Drag And Drop:Reorder</Link>
                 </nav>
                 <Routes>
                     <Route path="/MachineCode1" element={<Counter/>}/>
                     <Route path="/MachineCode2" element={<TodoApp/>}/>
                     <Route path="/MachineCode3" element={<ShoppingCart/>}/>
+                    <Route path="/MachineCode4" element={<DragAndDrop/>} />
+                    
                 </Routes>
             </div>
         </Suspense>
